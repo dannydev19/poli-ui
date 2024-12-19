eaa73e77c16375c02b0692f9715381fae3a2ee26
Danilo Nunez <danilonunez@Dani-MBP.localdomain>
2024-12-19 10:34:20 -0300

    login with slack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,10 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        <div>
+          [Login with Slack]
+        </div>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -48,7 +52,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
+      </main >
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -96,6 +100,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </div >
   );
 }
